bc488bb24dd3122cf0781910322c53384706ed93
Elgun <elgunam@code.edu.az>
2022-04-30 22:08:57 +0400

    Add project files.

---
+++ BookSystem.App/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+using System.Threading;
+
+namespace BookSystem.App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Init();
+        }
+
+        private static void Init()
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.Title = "Book System v1.0";
+
+            CultureInfo ci = new CultureInfo("az-Latn-Az");
+            ci.NumberFormat.NumberDecimalSeparator = ".";
+
+            Thread.CurrentThread.CurrentCulture = ci;
+            Thread.CurrentThread.CurrentUICulture = ci;
+        }
+    }
+}
---
+++ BookSystem.Lib/Helpers.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace BookSystem.Lib
+{
+    public class Helpers
+    {
+        public static string ReadString(string caption,bool required=false)
+        {
+        l1:
+            Console.Write(caption);
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            string value = Console.ReadLine();
+            Console.ResetColor();
+            if (required==true && string.IsNullOrWhiteSpace(value))
+            {
+                PrintError("Bosh buraxila bilmez");
+                goto l1;
+            }
+            return value;
+        }
+
+        public static int ReadInt(string caption, int minValue = 0)
+        {
+            l1:
+            Console.Write(caption);
+            string value = Console.ReadLine();
+
+            if (!int.TryParse(value,out int number))
+            {
+                PrintError("Duzgun eded daxil edilmeyib");
+                goto l1;
+            }
+            else if(minValue > number)
+            {
+                PrintError($"Minimal {minValue} daxil edile biler");
+                goto l1;
+            }
+            return number;
+        }
+
+        public static double ReadDouble(string caption, double minValue = 0)
+        {
+        l1:
+            Console.Write(caption);
+            string value = Console.ReadLine();
+            double number;
+            if (!double.TryParse(value, out  number))
+            {
+                PrintError("Duzgun eded daxil edilmeyib");
+                goto l1;
+            }
+            else if (minValue > number)
+            {
+                PrintError($"Minimal {minValue} daxil edile biler");
+                goto l1;
+            }
+            return number;
+        }
+
+        public static void PrintError(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+}
